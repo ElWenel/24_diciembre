5a0becba314a36bcbbb6fe58e61573777b72165c
ElWenel <elwenel@gmail.com>
2025-12-28 00:01:03 -0500

    Actualizar hora objetivo a 1 AM hora peruana

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Copos de nieve animados
-const targetDate = new Date(Date.UTC(2025, 11, 24, 6, 0, 0)); // 24 de diciembre 2025, 01:00 hora peruana (UTC-5)
+const targetDate = new Date(Date.UTC(2025, 11, 29, 6, 0, 0)); // 24 de diciembre 2025, 01:00 hora peruana (UTC-5)
 function crearCoposNieve() {
   const snowContainer = document.querySelector(".snow");
   if (!snowContainer) return;
